089f7aa71fc550085ef24a47329ecde2d6b7f1af
sokratgruzit <tavadzed@gmail.com>
2025-08-26 03:29:20 +0400

    last

--- src/client.ts
+++ src/client.ts
@@ -1,3 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+declare global {
+  // добавляем поле prisma в глобальный объект
+  var prisma: PrismaClient | undefined;
+}
+
+const client = globalThis.prisma || new PrismaClient();
+if (!globalThis.prisma) globalThis.prisma = client;
+
+export const prisma = client;
